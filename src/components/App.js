6ac288504a8c09c574e52e3ed41af7fb956db29d
YathaarthaAakashLabs <yathaartha.aakashlabs@gmail.com>
2022-05-08 11:33:16 +0545

    added video

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./home/HomePage";
+import Header from "./layout/Header";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route exact path="/" element={<HomePage />} />
       </Routes>
--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-import Header from "../layout/Header";
 
 const HomePage = () => {
   return (
-    <div>
-      <Header />
+    <div className="home-page page">
+      <div className="page-header">
+        <video id="home-video" loop muted autoPlay>
+          <source
+            src="https://aakash.group/wp-content/uploads/2019/10/aakash-group-text-anime-change1.mp4"
+            type="video/mp4"
+          />
+          Your browser does not support HTML5 video.
+        </video>{" "}
+      </div>
     </div>
   );
 };
